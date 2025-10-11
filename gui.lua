09a8dc0d0646af29f329fc8443771340dceca396
Andrey Stepanov <standmit@yandex.ru>
2025-10-11 18:00:18 +0500

    fix: allow replacing stale entries left after crash

--- gui.lua
+++ gui.lua
@@ -2,6 +2,9 @@ local S = minetest.get_translator("apartment")
 local gui = flow.widgets
 local p = {}
 
+local function isEqual(pos1, pos2)
+	return (pos1.x == pos2.x) and (pos1.y == pos2.y) and (pos1.z == pos2.z)
+end
 
 p.configure_gui = flow.make_gui(function(player, ctx)
 	local name = player:get_player_name()
@@ -124,7 +127,7 @@ p.configure_gui = flow.make_gui(function(player, ctx)
 							return
 						end
 
-						if apartment.apartments[category] and apartment.apartments[category][descr] then
+						if apartment.apartments[category] and apartment.apartments[category][descr] and (not isEqual(apartment.apartments[category][descr].pos, pos)) then
 							minetest.chat_send_player(name,
 								S("Error: The apartment @1@@@2 already exists. " ..
 								  "Please choose a different name or category.",
